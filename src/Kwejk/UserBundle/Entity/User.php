914a2602e81274ebe7e5d83d55ebe8bd9560bd6a
sith5 <sith5@o2.pl>
2014-10-26 10:17:11 +0100

    Bundle add 2

---
+++ src/Kwejk/UserBundle/Entity/User.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Kwejk\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Kwejk\MemsBundle\Entity\Mem", mappedBy="createdBy")
+     * @var ArrayCollection
+     */ 
+    private $mems;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Kwejk\MemsBundle\Entity\Comment", mappedBy="createdBy")
+     * @var ArrayCollection
+     */ 
+    private $comments;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Kwejk\MemsBundle\Entity\Rating", mappedBy="createdBy")
+     * @var ArrayCollection
+     */ 
+    private $ratings;
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Add mems
+     *
+     * @param \Kwejk\MemsBundle\Entity\Mem $mems
+     * @return User
+     */
+    public function addMem(\Kwejk\MemsBundle\Entity\Mem $mems)
+    {
+        $this->mems[] = $mems;
+
+        return $this;
+    }
+
+    /**
+     * Remove mems
+     *
+     * @param \Kwejk\MemsBundle\Entity\Mem $mems
+     */
+    public function removeMem(\Kwejk\MemsBundle\Entity\Mem $mems)
+    {
+        $this->mems->removeElement($mems);
+    }
+
+    /**
+     * Get mems
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getMems()
+    {
+        return $this->mems;
+    }
+
+    /**
+     * Add comments
+     *
+     * @param \Kwejk\MemsBundle\Entity\Comment $comments
+     * @return User
+     */
+    public function addComment(\Kwejk\MemsBundle\Entity\Comment $comments)
+    {
+        $this->comments[] = $comments;
+
+        return $this;
+    }
+
+    /**
+     * Remove comments
+     *
+     * @param \Kwejk\MemsBundle\Entity\Comment $comments
+     */
+    public function removeComment(\Kwejk\MemsBundle\Entity\Comment $comments)
+    {
+        $this->comments->removeElement($comments);
+    }
+
+    /**
+     * Get comments
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getComments()
+    {
+        return $this->comments;
+    }
+
+    /**
+     * Add ratings
+     *
+     * @param \Kwejk\MemsBundle\Entity\Rating $ratings
+     * @return User
+     */
+    public function addRating(\Kwejk\MemsBundle\Entity\Rating $ratings)
+    {
+        $this->ratings[] = $ratings;
+
+        return $this;
+    }
+
+    /**
+     * Remove ratings
+     *
+     * @param \Kwejk\MemsBundle\Entity\Rating $ratings
+     */
+    public function removeRating(\Kwejk\MemsBundle\Entity\Rating $ratings)
+    {
+        $this->ratings->removeElement($ratings);
+    }
+
+    /**
+     * Get ratings
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getRatings()
+    {
+        return $this->ratings;
+    }
+}
